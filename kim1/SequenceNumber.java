687ddd1b60c9240d3489daed0833473b03cca9b1
Sum-Sovann2018 <sumsovann18@gmail.com>
2019-05-23 13:20:25 +0700

    println

---
+++ kim1/SequenceNumber.java
@@ -0,0 +1,14 @@
+public class SequenceNumber {
+    public static void main(String[] args){
+        int  i,j;
+        for(i=1;i<=5;i++)
+        {
+            for(j=1;j<5;j++)
+            {
+                System.out.println(j);
+            }
+            System.out.println(" ");
+        }
+
+    }
+}
